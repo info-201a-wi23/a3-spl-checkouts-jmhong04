f96b8c1faea084380daf4d2dd8016c7195286297
jmhong04 <jmhong04@uw.edu>
2023-02-25 17:15:23 -0800

    fix chart 1 code completely

--- chart1.R
+++ chart1.R
@@ -8,20 +8,27 @@ jg_df <- read.csv("John_Green_Checkouts.csv", stringsAsFactors = FALSE)
 # create a variable of only Ebooks with English titles
 engl_titles <- c("An Abundance of Katherines", "Let It Snow: Three Holiday Romances", "Looking for Alaska", "Paper Towns", "The Anthropocene Reviewed: Essays on a Human-Centered Planet", "The Fault in Our Stars", "Turtles All the Way Down", "Will Grayson, Will Grayson")
 
-# create new dataframe and filter Title to English titles
+# create new dataframe and with a new date column
 jgdf_chart1 <- jg_df %>%
-  filter(Title %in% engl_titles)
+  mutate(date  = paste0(CheckoutYear, "-", CheckoutMonth,  "-01" ))
 
-# create line plot of Year v. Number of Checkouts for John Green's English e-books
+jgdf_chart1$date <- as.Date(jgdf_chart1$date, format = "%Y-%m-%d")
+
+# filter this new dataframe to English titles and find the sum checkouts for each month of each year
+jgdf_chart1 <- jgdf_chart1 %>% 
+  filter(Title %in% engl_titles, na.rm = TRUE) %>% 
+  group_by(Title, date) %>% 
+  summarize(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+
+# create line plot of Date v. Number of Checkouts for John Green's English e-books
 ggplot(data = jgdf_chart1) +
   geom_line(mapping = aes(
-    x = CheckoutYear,
-    y = Checkouts,
+    x = date,
+    y = sum_checkouts,
     color = Title
   )) +
   labs(
-    title = "Number of Checkouts Overtime for John Green's Ebooks (English versions)",
-    x = "Year of Ebook Checkout",
+    title = "Total Checkouts per Month for John Green's Ebooks (English versions)",
+    x = "Date of Ebook Checkout",
     y = "Number of Checkouts",
-    color = "Title of Ebook"
-  )
+    color = "Title of Ebook")

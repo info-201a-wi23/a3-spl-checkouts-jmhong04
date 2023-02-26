58c2d262927ac529fe12cd8634d31e12b4e94751
jmhong04 <jmhong04@uw.edu>
2023-02-26 09:38:38 -0800

    add chart 3 and paragraph

--- chart3.R
+++ chart3.R
@@ -2,29 +2,32 @@
 library(ggplot2)
 library(tidyverse)
 library(openintro)
+library("scales")
+library("dplyr")
+library("ggplot2")
+library(stringr)
 
 jg_df <- read.csv("John_Green_Checkouts.csv", stringsAsFactors = FALSE)
 
 # create new dataframe and with a new date column
-jgdf_chart2 <- jg_df %>%
+jgdf_chart3 <- jg_df %>%
   mutate(date  = paste0(CheckoutYear, "-", CheckoutMonth,  "-01" ))
 
-jgdf_chart2$date <- as.Date(jgdf_chart2$date, format = "%Y-%m-%d")
+jgdf_chart3$date <- as.Date(jgdf_chart3$date, format = "%Y-%m-%d")
 
 # filter this new dataframe to English titles and find the sum checkouts for each month of each year
-jgdf_chart2 <- jgdf_chart2 %>% 
-  group_by(MaterialType, date) %>% 
-  summarize(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+jgdf_chart3 <- jgdf_chart3 %>% 
+  filter(str_detect(Title, "(?i)stars") | str_detect(Title, "(?i)estrella"), na.rm = TRUE)
 
 # create line plot of Date v. Number of Checkouts for John Green's English e-books
-ggplot(data = jgdf_chart2) +
-  geom_line(mapping = aes(
-    x = date,
-    y = sum_checkouts,
-    color = MaterialType
-  )) +
+ggplot(data = jgdf_chart3) +
+  geom_col(aes(x = CheckoutYear, y = Checkouts,
+               fill = MaterialType)) + 
+  scale_x_continuous(breaks = seq(2006, 2023, 1)) + 
   labs(
-    title = "Total Checkouts per Month for John Green's Works by Material Type",
-    x = "Date of Checkout",
+    title = 'Total Checkouts per Year for "The Fault in Our Stars"',
+    x = "Year of Checkout",
     y = "Number of Checkouts",
-    color = "Material Type")
+    fill = "Material Type")
+
+

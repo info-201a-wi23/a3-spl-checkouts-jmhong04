9821db61aa0fbad805bae284aa7c8b193b83306f
jmhong04 <jmhong04@uw.edu>
2023-02-26 17:49:15 -0800

    edit chart 3 and paragraph

--- chart2.R
+++ chart2.R
@@ -5,26 +5,24 @@ library(openintro)
 
 jg_df <- read.csv("John_Green_Checkouts.csv", stringsAsFactors = FALSE)
 
-# create new dataframe and with a new date column
+# group the number of checkouts for each year and for each material type in a new dataframe
+# find the sum checkouts for each year
 jgdf_chart2 <- jg_df %>%
-  mutate(date  = paste0(CheckoutYear, "-", CheckoutMonth,  "-01" ))
-
-jgdf_chart2$date <- as.Date(jgdf_chart2$date, format = "%Y-%m-%d")
-
-# filter this new dataframe to English titles and find the sum checkouts for each month of each year
-jgdf_chart2 <- jgdf_chart2 %>% 
-  group_by(MaterialType, date) %>% 
+  filter(MaterialType != "REGPRINT") %>%
+  group_by(CheckoutYear, MaterialType) %>%
   summarize(sum_checkouts = sum(Checkouts, na.rm = TRUE))
 
-# create line plot of Date v. Number of Checkouts for John Green's English e-books
+# create line plot of Year v. Number of Checkouts for John Green's works per Material Type
 ggplot(data = jgdf_chart2) +
   geom_line(mapping = aes(
-    x = date,
+    x = CheckoutYear,
     y = sum_checkouts,
     color = MaterialType
   )) +
   labs(
-    title = "Total Checkouts per Month for John Green's Works by Material Type",
-    x = "Date of Checkout",
+    title = "Total Annual Checkouts of John Green's Works in the SPL per Material Type",
+    x = "Year of Checkout",
     y = "Number of Checkouts",
-    color = "Material Type")
+    color = "Material Type"
+  ) +
+  scale_x_continuous(breaks = seq(2005, 2023, 2))
